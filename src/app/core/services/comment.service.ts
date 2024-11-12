d1a8aedfdf505881cb2e0dab5cc4048bd5d54baf
duongnh <duonghd1803@gmail.com>
2024-11-12 08:14:11 +0700

    feat: add pagination

---
+++ src/app/core/services/comment.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { baseUrl } from './constant';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class CommentService {
+  protected readonly http = inject(HttpClient);
+  constructor() {}
+  create(body: any) {
+    return this.http.post(baseUrl + 'Comment', body);
+  }
+  update(body: any) {
+    return this.http.put(baseUrl + 'Comment/' + body.id, body);
+  }
+  getAll(params: any) {
+    return this.http.get(baseUrl + 'Comment/Paging', { params });
+  }
+  getByProductId(id: any) {
+    return this.http.get(baseUrl + 'Comment/'+id, );
+  }
+
+}
